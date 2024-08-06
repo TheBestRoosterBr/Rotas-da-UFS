4bd28a1ea1be19381228d59337c71e943ba50de2
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-06 09:56:32 -0300

    feat: leitura das variáveis de ambiente de um arquivo .env

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,18 @@
 import os
 from typing import cast
 
+from dotenv import load_dotenv
+
 from api import create_app
 
 
 def main():
+    # Load environment variables from .env file(s)
+    load_dotenv('.env')
+
+    if os.environ.get('SERVER_RUN', default='dev') == 'prod':
+        print('This server shouldn\'t be used on production environment')
+        return
 
     # Port of debug server
     SERVER_PORT = os.environ.get('SERVER_PORT', default=5000)
