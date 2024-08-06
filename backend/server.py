032e1e05a48e8fd61515150ea7c78b1f9a043792
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-06 20:29:27 -0300

    feat: server script atualizado

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 import os
+import os.path
 from typing import cast
 
 from dotenv import load_dotenv
@@ -8,20 +9,25 @@ from api import create_app
 
 def main():
     # Load environment variables from .env file(s)
-    load_dotenv('.env')
+    if os.path.isfile('.env'):
+        load_dotenv('.env')
 
-    if os.environ.get('SERVER_RUN', default='dev') == 'prod':
-        print('This server shouldn\'t be used on production environment')
-        return
-
-    # Port of debug server
+    # Server configuration environment variables
+    SERVER_RUN = os.environ.get('SERVER_RUN', default='dev')
     SERVER_PORT = os.environ.get('SERVER_PORT', default=5000)
 
-    # Debug server host
-    SERVER_HOST = os.environ.get('SERVER_HOST', default='127.0.0.1')
+    if SERVER_RUN == 'dev':
+        # When on dev environment "listening" on all interfaces by default,
+        # particularly useful when running on a container.
+        SERVER_HOST = os.environ.get('SERVER_HOST', default='0.0.0.0')
+    else:
+        SERVER_HOST = os.environ.get('SERVER_HOST', default='127.0.0.1')
 
-    api = create_app(None)
-    api.run(host=SERVER_HOST, port=cast(int, SERVER_PORT), debug=True)
+    if SERVER_RUN == 'dev':
+        api = create_app(None)
+        api.run(host=SERVER_HOST, port=cast(int, SERVER_PORT), debug=True)
+    else:
+        pass
 
 
 if __name__ == '__main__':
