a198c22cbc28090d443be2f15f9556b794aed6d2
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-06 21:08:24 -0300

    feat: da para acessar o backend pelo container :)

--- backend/server.py
+++ backend/server.py
@@ -20,14 +20,41 @@ def main():
         # When on dev environment "listening" on all interfaces by default,
         # particularly useful when running on a container.
         SERVER_HOST = os.environ.get('SERVER_HOST', default='0.0.0.0')
-    else:
+    elif SERVER_RUN == 'prod':
         SERVER_HOST = os.environ.get('SERVER_HOST', default='127.0.0.1')
+    else:
+        return
+
+    # API
+    api = create_app(None)
 
     if SERVER_RUN == 'dev':
-        api = create_app(None)
         api.run(host=SERVER_HOST, port=cast(int, SERVER_PORT), debug=True)
+    elif SERVER_RUN == 'prod':
+        from multiprocessing import cpu_count
+        from gunicorn.app.base import BaseApplication
+
+        class GunicornApp(BaseApplication):
+            def __init__(self, app, options=None):
+                self.options = options or {}
+                self.app = app
+
+                super().__init__()
+
+            def load_config(self):
+                for key, value in self.options.items():
+                    if key in self.cfg.settings and value is not None:
+                        self.cfg.set(key.lower(), value)
+
+            def load(self):
+                return self.app
+
+        GunicornApp(api, {
+            'bind': f'{SERVER_HOST}:{SERVER_PORT}',
+            'workers': (cpu_count() * 1) + 1,
+        }).run()
     else:
-        pass
+        return
 
 
 if __name__ == '__main__':
