14069607f1451523b76202de181a72df4b334c46
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-08 12:10:25 -0300

    feat: setup do front-end

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+    content: [
+        './src/**/*.html',
+
+        './src/**/*.js',
+        './src/**/*.jsx',
+
+        './src/**/*.ts',
+        './src/**/*.tsx',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: 'Inter',
+            },
+
+            boxShadow: {
+                shadow: '0px 8px 8px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px 1px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.03), inset 0px 1px 0px rgba(255, 255, 255, 0.03)',
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config;
+
