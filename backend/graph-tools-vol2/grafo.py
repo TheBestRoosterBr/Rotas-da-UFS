94dd70e5403a566f656b32fd2db2abb1a2039aad
WalterHenri <walterdoideira5438@gmail.com>
2024-08-09 11:41:07 -0300

    modificado arquivo grafo para suportar diversos construtores

--- backend/graph-tools-vol2/grafo.py
+++ backend/graph-tools-vol2/grafo.py
@@ -2,3 +2,7 @@ class Grafo:
     def __init__(self):
         self.estados = []
         self.transicoes = []
+
+    def __init__(self, estados, transicoes):
+        self.estados = estados
+        self.transicoes = transicoes
