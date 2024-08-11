1270bc797567ef8b6457ddb213b23dafe03a716c
Carol <carolinevivaslins@gmail.com>
2024-08-11 14:10:37 -0300

    BuscaEmLargura implementada. closed #6

--- backend/graph-tools-vol2/BuscaEmLargura.py
+++ backend/graph-tools-vol2/BuscaEmLargura.py
@@ -1,6 +1,30 @@
 class BuscaEmLargura:
     def __init__(self):
-        pass
+        self.queue= []
+        self.visitados= set()
+        self.caminho= []
 
-    def buscar(self, grafo, inicio, fim):
-        pass
+    def busca(self, grafo, inicio, fim):
+        self.queue.append(inicio)
+        self.visitados.add(inicio)
+        pai= {inicio: None}
+
+        while self.queue:
+            estado_atual= self.queue.pop(0)
+
+            if estado_atual == fim:
+                self.caminho= []
+
+                while estado_atual is not None:
+                    self.caminho.append(estado_atual)
+                    estado_atual= pai[estado_atual]
+
+                self.caminho.reverse()
+                return self.caminho
+
+            for transicao in grafo.transicoes:
+                if transicao.origem== estado_atual and transicao.destino not in self.visitados:
+                    self.queue.append(transicao.destino)
+                    self.visitados.add(transicao.destino)
+                    pai[transicao.destino]= estado_atual
+        return None
