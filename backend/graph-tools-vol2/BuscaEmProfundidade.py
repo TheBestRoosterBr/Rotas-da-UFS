38307f1cb60cafcabf0639df3e2d917ade32f7d0
marly <marlyssonsd@gmail.com>
2024-08-12 11:05:12 -0300

    recursão?

--- backend/graph-tools-vol2/BuscaEmProfundidade.py
+++ backend/graph-tools-vol2/BuscaEmProfundidade.py
@@ -1,7 +1,20 @@
 class BuscaEmProfundidade:
     def __init__(self):
-        pass
+        self.visitados = set()
+        self.caminho = []
 
-    def buscar(self, grafo, inicio, fim):
-        pass
-    
+    def busca(self, grafo, inicio, fim):
+        self.visitados.add(inicio)
+        self.caminho.append(inicio)
+
+        if inicio == fim:
+            return self.caminho  
+
+        for transicao in grafo.transicoes:
+            if transicao.origem == inicio and transicao.destino not in self.visitados:
+                resultado = self.busca(grafo, transicao.destino, fim)
+                if resultado:
+                    return resultado
+
+        self.caminho.pop()
+        return None
