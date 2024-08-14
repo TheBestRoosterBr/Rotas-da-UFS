65f5c5deb4749d15a28a1b86438e7057b765ecdc
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-14 02:25:21 -0300

    primeiro modal incluido, o da localização atual

--- frontend/src/app/pages/home/index.tsx
+++ frontend/src/app/pages/home/index.tsx
@@ -1,9 +1,12 @@
 import {
     ArrowRight,
+    Funnel,
+    MagnifyingGlass,
     MapPin,
     Moon,
     Sun,
-    Swap
+    Swap,
+    X
 } from '@phosphor-icons/react';
 
 import {
@@ -12,12 +15,38 @@ import {
 } from 'react';
 
 
+interface Location {
+    id: number;
+    name: string;
+}
+
+
 export function HomePage(): ReactNode {
     const [theme, setTheme] = useState<string>(
         localStorage.getItem('themeMode') ?? 'dark'
     );
 
-    const [startLocation, setStartLocation] = useState<number>(0);
+    // Per-modal
+    const [location, setLocation] = useState<Location | null>(null);
+
+    // Window states
+    const [whereLocation, setWhereLocation] = useState<number>(0);
+    const [isWhereModelOpen, setWhereModalOpen] = useState<boolean>(true);
+
+    const [locationsList, setLocationsList] = useState<Location[]>([
+        {
+            id: 1,
+            name: 'sala 007',
+        },
+        {
+            id: 2,
+            name: 'sala 006',
+        },
+        {
+            id: 3,
+            name: 'sala 005',
+        },
+    ]);
 
     function changeTheme(): void {
         const newTheme: string = theme == 'dark' ? 'light' : 'dark';
@@ -109,6 +138,101 @@ export function HomePage(): ReactNode {
                     </div>
                 </div>
             </main>
+
+            {isWhereModelOpen && (
+                <div className='fixed inset-0 bg-black/80 flex justify-center items-center'>
+                    {/* Modal window */}
+                    <div className='flex flex-col w-[720px] h-96 bg-zinc-900 px-6 py-5 rounded-xl space-y-5'>
+                        {/* Title row */}
+                        <div className="flex items-center justify-between">
+                            <span className='text-lg text-zinc-300 font-semibold'>
+                                Nós diga onde você está
+                            </span>
+                            <button
+                                onClick={() => setWhereModalOpen(false)}
+                                className='p-1 bg-zinc-800 rounded-full'>
+                                <X
+                                    className='size-5 text-zinc-400' />
+                            </button>
+                        </div>
+
+                        {/* Content row */}
+                        <div className='flex space-x-4'>
+                            {/* Locations column */}
+                            <div className='space-y-6'>
+                                {/* Search & Filter & Selected location */}
+                                <div className='space-y-3'>
+                                    {/* Search & Filter */}
+                                    <div className='flex flex-row space-x-2 items-center justify-center'>
+                                        {/* Search input */}
+                                        <div className='flex items-center h-8 space-x-2 p-[10px] rounded-lg dark:bg-zinc-800'>
+                                            <input
+                                                type='text'
+                                                placeholder='Procurar local...'
+                                                className='min-w-52 text-sm bg-transparent outline-none text-zinc-300 placeholder:text-zinc-300'
+                                                />
+
+                                            <MagnifyingGlass
+                                                className='size-6 text-zinc-300' />
+                                        </div>
+
+                                        {/* Filter button */}
+                                        <button
+                                            className='p-1 bg-zinc-800 rounded-lg'>
+                                            <Funnel
+                                                className='size-6 text-zinc-300' />
+                                        </button>
+                                    </div>
+
+                                    {location !== null && (
+                                        <div
+                                            key={location.id}
+                                            className='flex item-center justify-between rounded-lg px-4 py-1 h-8 text-start  dark:bg-cyan-300'>
+                                            {location.name}
+
+                                            <button
+                                                onClick={() => setLocation(null)}
+                                                className=''>
+                                                <X
+                                                    className='size-5 text-cyan-950' />
+                                            </button>
+                                        </div>
+                                    )}
+                                </div>
+
+                                {/* Locations */}
+                                {/* TODO: scrollable list */}
+                                <div className='flex flex-col space-y-2 overflow-y-auto'>
+                                    {locationsList.map((loc) => {
+                                        if (location !== null
+                                                && loc.id === location.id)
+                                            return null;
+
+                                        return (
+                                            <button
+                                                key={loc.id}
+                                                onClick={() => setLocation(loc)}
+                                                className='rounded-lg px-4 py-1 h-8 text-start text-zinc-300 bg-zinc-800'>
+                                                {loc.name}
+                                            </button>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+
+                            {/* Vertical separator */}
+                            {/* TODO: expand separator to full height */}
+                            <div
+                                className='w-px h-full bg-zinc-600'
+                                />
+
+                            {/* Preview column */}
+                            <div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
