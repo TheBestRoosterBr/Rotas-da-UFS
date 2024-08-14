0c8e5190152e7ba277538c70810baf95b8a804af
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-14 00:27:22 -0300

    atualização da font do front-end

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: 'Inter',
+                sans: 'JetBrains Mono',
             },
 
             boxShadow: {
