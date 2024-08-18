ee041e524633184c7dd535edabf8daaa5ecb5f54
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-18 10:45:41 -0300

    correções

--- backend/server.py
+++ backend/server.py
@@ -48,6 +48,31 @@ def main():
         return
 
 
+def find_git_directory(start_path):
+    current_path = start_path
+    while current_path != os.path.dirname(current_path):
+        if os.path.isdir(os.path.join(current_path, '.git')):
+            return current_path
+        current_path = os.path.dirname(current_path)
+    return None
+
+
+def install_pre_commit_hook():
+    hook_code = """#!/bin/sh
+if git diff --cached --name-only '*.py' | xargs grep -q 'import backend.'; then
+    echo "Erro: dar commit com 'import backend.' não é permitido!"
+    exit 1
+fi
+"""
+    git_hooks_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.git', 'hooks')
+    hook_file_path = os.path.join(git_hooks_dir, 'pre-commit')
+
+    with open(hook_file_path, 'w') as hook_file:
+        hook_file.write(hook_code)
+
+    os.chmod(hook_file_path, 0o755)
+
 if __name__ == '__main__':
+    install_pre_commit_hook()
     main()
 
